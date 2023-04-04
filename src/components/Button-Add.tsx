d22c2608a7ae5ac4b23bb4146e587ff49da243c2
Rek Dev <rekdev@keemail.me>
2023-04-04 15:07:34 -0600

    Varius ux changes

---
+++ src/components/Button-Add.tsx
@@ -0,0 +1,11 @@
+import { TbPlus } from "react-icons/tb";
+
+function ButtonAdd() {
+  return (
+    <button className="button-add">
+      <TbPlus />
+    </button>
+  );
+}
+
+export default ButtonAdd;
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,9 +1,11 @@
+import ButtonAdd from "../components/Button-Add";
 import Task from "../components/Task";
 
 function Home() {
   return (
     <>
       <Task title="Tarea 1" completed={false} />
+      <ButtonAdd />
     </>
   );
 }
